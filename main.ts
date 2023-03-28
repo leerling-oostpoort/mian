211f375be79c371d65970b135a6008fd8d6ef53f
Leerling Oostpoort <129111356+leerling-oostpoort@users.noreply.github.com>
2023-03-28 19:09:58 +0200

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,15 +1,13 @@
 input.onButtonPressed(Button.A, function () {
-    basic.showIcon(IconNames.Target)
-    basic.showIcon(IconNames.SmallDiamond)
+    basic.showArrow(ArrowNames.North)
 })
 input.onButtonPressed(Button.AB, function () {
-    basic.showNumber(8)
     basic.showLeds(`
-        . . . . .
-        # # . # #
-        # . # . #
-        # . . . #
-        # . . . #
+        # . . # .
+        # . . # .
+        # # # # .
+        # . . # .
+        # . . # .
         `)
     basic.showLeds(`
         . . # . .
@@ -19,31 +17,37 @@ input.onButtonPressed(Button.AB, function () {
         . . # . .
         `)
     basic.showLeds(`
+        . . . # .
+        . . # . .
         . . # # .
         . . . # .
-        # # # # .
-        # . . # .
-        . # # # .
+        . . # . .
         `)
     basic.showLeds(`
-        # . . . .
-        # # # # .
-        # . . # .
-        # . . # .
+        # . # . .
+        # # . . .
+        # # . . .
+        # . # . .
         # . . # .
         `)
+    basic.showLeds(`
+        . # # # .
+        . # . # .
+        . # # # .
+        . # . . .
+        . # # # .
+        `)
+    basic.showIcon(IconNames.Heart)
 })
 input.onButtonPressed(Button.B, function () {
-    basic.showIcon(IconNames.Yes)
-    basic.showIcon(IconNames.No)
+    basic.showArrow(ArrowNames.South)
 })
-basic.showNumber(8)
 basic.showLeds(`
-    . . . . .
-    # # . # #
-    # . # . #
-    # . . . #
-    # . . . #
+    # . . # .
+    # . . # .
+    # # # # .
+    # . . # .
+    # . . # .
     `)
 basic.showLeds(`
     . . # . .
@@ -53,22 +57,24 @@ basic.showLeds(`
     . . # . .
     `)
 basic.showLeds(`
+    . . . # .
+    . . # . .
     . . # # .
     . . . # .
-    # # # # .
-    # . . # .
-    . # # # .
+    . . # . .
     `)
 basic.showLeds(`
-    # . . . .
-    # # # # .
-    # . . # .
-    # . . # .
+    # . # . .
+    # # . . .
+    # # . . .
+    # . # . .
     # . . # .
     `)
-basic.forever(function () {
-    basic.showIcon(IconNames.Heart)
-    basic.showIcon(IconNames.Happy)
-    basic.clearScreen()
-    basic.clearScreen()
-})
+basic.showLeds(`
+    . # # # .
+    . # . # .
+    . # # # .
+    . # . . .
+    . # # # .
+    `)
+basic.showIcon(IconNames.Heart)
